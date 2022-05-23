8fd62b386d226543bb4a3ebba55b68a91a8ce039
andy840119 <andy840119@gmail.com>
2022-05-23 22:43:51 +0800

    Add the text index and it's test case.

---
+++ LrcParser.Tests/Model/TextIndexTest.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using LrcParser.Model;
+using NUnit.Framework;
+
+namespace LrcParser.Tests.Model;
+
+[TestFixture]
+public class TextIndexTest
+{
+    [TestCase(1, 1)]
+    [TestCase(1.5, 1.5)]
+    [TestCase(-1.5, -1.5)]
+    public void TestOperatorEqual(double tone1, double tone2)
+    {
+        Assert.AreEqual(numberToTextIndex(tone1), numberToTextIndex(tone2));
+    }
+
+    [TestCase(-1, 1)]
+    [TestCase(1, -1)]
+    [TestCase(1.5, -1.5)]
+    [TestCase(1.5, 1)]
+    [TestCase(-1.5, -1)]
+    [TestCase(-1.5, -2)]
+    public void TestOperatorNotEqual(double tone1, double tone2)
+    {
+        Assert.AreNotEqual(numberToTextIndex(tone1), numberToTextIndex(tone2));
+    }
+
+    [TestCase(1, 0, true)]
+    [TestCase(1, 0.5, true)]
+    [TestCase(1, 1, false)]
+    [TestCase(1, 1.5, false)]
+    public void TestOperatorGreater(double tone1, double tone2, bool match)
+    {
+        Assert.AreEqual(numberToTextIndex(tone1) > numberToTextIndex(tone2), match);
+    }
+
+    [TestCase(1, 0, true)]
+    [TestCase(1, 0.5, true)]
+    [TestCase(1, 1, true)]
+    [TestCase(1, 1.5, false)]
+    public void TestOperatorGreaterOrEqual(double tone1, double tone2, bool match)
+    {
+        Assert.AreEqual(numberToTextIndex(tone1) >= numberToTextIndex(tone2), match);
+    }
+
+    [TestCase(-1, 0, true)]
+    [TestCase(-1, -0.5, true)]
+    [TestCase(-1, -1, false)]
+    [TestCase(-1, -1.5, false)]
+    public void TestOperatorLess(double tone1, double tone2, bool match)
+    {
+        Assert.AreEqual(numberToTextIndex(tone1) < numberToTextIndex(tone2), match);
+    }
+
+    [TestCase(-1, 0, true)]
+    [TestCase(-1, -0.5, true)]
+    [TestCase(-1, -1, true)]
+    [TestCase(-1, -1.5, false)]
+    public void TestOperatorLessOrEqual(double tone1, double tone2, bool match)
+    {
+        Assert.AreEqual(numberToTextIndex(tone1) <= numberToTextIndex(tone2), match);
+    }
+
+    private static TextIndex numberToTextIndex(double tone)
+    {
+        var half = Math.Abs(tone) % 1 == 0.5;
+        var scale = tone < 0 ? (int)tone - (half ? 1 : 0) : (int)tone;
+        return new TextIndex(scale, half ? IndexState.End : IndexState.Start);
+    }
+}
