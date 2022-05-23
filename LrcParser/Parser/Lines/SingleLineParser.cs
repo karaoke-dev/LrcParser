583f0eacbbaac07d1750e29ee889a35e7ce02689
andy840119 <andy840119@gmail.com>
2022-05-23 23:11:34 +0800

    Implement single line parser for convert single line string to object.

---
+++ LrcParser/Parser/Lines/SingleLineParser.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace LrcParser.Parser.Lines;
+
+/// <inheritdoc>
+/// Base component pass string
+/// </inheritdoc>
+/// <typeparam name="T">Encode and decode object type</typeparam>
+public abstract class SingleLineParser<T> : SingleLineParser<T,string>
+{
+
+}
+
+/// <summary>
+/// Base component pass target type of object and get expect return type
+/// </summary>
+/// <typeparam name="T">Encode and decode object type.</typeparam>
+/// <typeparam name="TR">Encode and decode source type.</typeparam>
+public abstract class SingleLineParser<T,TR>
+{
+    /// <summary>
+    /// Decode to target class and leave remain text
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    public abstract T Decode(TR text);
+
+    /// <summary>
+    /// Encode target component
+    /// </summary>
+    /// <param name="component"></param>
+    /// <returns></returns>
+    public abstract TR Encode(T component);
+}
