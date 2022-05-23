2bd53b4f6cac04fdd2795d0ecc39ed876d3c09ec
andy840119 <andy840119@gmail.com>
2022-05-23 22:44:33 +0800

    Add the remain models.

---
+++ LrcParser/Model/Lyric.cs
@@ -0,0 +1,17 @@
+// Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace LrcParser.Model;
+
+public class Lyric
+{
+    /// <summary>
+    /// Text
+    /// </summary>
+    public string Text { get; set; }
+
+    /// <summary>
+    /// Time tags
+    /// </summary>
+    public SortedDictionary<TextIndex, int?> TimeTags { get; set; }
+}
