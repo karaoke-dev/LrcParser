8ff74dfbb5651b3bd43f2e1cbcc3fec106a81a38
andy840119 <andy840119@gmail.com>
2022-05-24 20:23:55 +0800

    Implement the helper for testing time-tag related things.

---
+++ LrcParser/Extension/RegexExtensions.cs
@@ -0,0 +1,18 @@
+// Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Text.RegularExpressions;
+using LrcParser.Utils;
+
+namespace LrcParser.Extension;
+
+internal static class RegexExtensions
+{
+    internal static TType GetGroupValue<TType>(this Match match, string key, bool useDefaultValueIfEmpty = true)
+    {
+        string? value = match.Groups[key]?.Value;
+
+        // if got empty value, should change to null.
+        return TypeUtils.ChangeType<TType>(string.IsNullOrEmpty(value) ? null : value);
+    }
+}
