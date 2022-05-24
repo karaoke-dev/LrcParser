89fc437a64d48e14c7f9503ff663594035c120f7
andy840119 <andy840119@gmail.com>
2022-05-24 22:52:09 +0800

    Create the lrc lyric for conversion.

---
+++ LrcParser/Parser/Lrc/Metadata/LrcLyric.cs
@@ -0,0 +1,19 @@
+// Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using LrcParser.Model;
+
+namespace LrcParser.Parser.Lrc.Metadata;
+
+public class LrcLyric
+{
+    /// <summary>
+    /// Text
+    /// </summary>
+    public string Text { get; set; } = "";
+
+    /// <summary>
+    /// Time tags
+    /// </summary>
+    public SortedDictionary<TextIndex, int?> TimeTags { get; set; } = new();
+}
