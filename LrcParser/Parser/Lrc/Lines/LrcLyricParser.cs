e033d7868d80efc9d44002ee2769aa4b07b066e3
andy840119 <andy840119@gmail.com>
2022-05-27 20:29:51 +0800

    Use the utils to replace the logic.

--- LrcParser/Parser/Lrc/Lines/LrcLyricParser.cs
+++ LrcParser/Parser/Lrc/Lines/LrcLyricParser.cs
@@ -1,12 +1,9 @@
 // Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Text.RegularExpressions;
-using LrcParser.Model;
 using LrcParser.Parser.Lines;
 using LrcParser.Parser.Lrc.Metadata;
 using LrcParser.Parser.Lrc.Utils;
-using LrcParser.Utils;
 
 namespace LrcParser.Parser.Lrc.Lines;
 
@@ -17,58 +14,16 @@ public class LrcLyricParser : SingleLineParser<LrcLyric>
 
     public override LrcLyric Decode(string text)
     {
-        if (!CanDecode(text))
-            return new LrcLyric();
-
-        var timeTagRegex = new Regex(@"\[\d\d:\d\d[:.]\d\d\]");
-        var matchTimeTags = timeTagRegex.Matches(text);
-
-        var endTextIndex = text.Length;
-
-        var lyric = new LrcLyric();
-        var startIndex = 0;
-
-        foreach (var match in matchTimeTags.ToArray())
+        var (lyric, timeTags) = LrcTimedTextUtils.TimedTextToObject(text);
+        return new LrcLyric
         {
-            var endIndex = match.Index;
-            if (startIndex < endIndex)
-            {
-                // add the lyric.
-                lyric.Text += text[startIndex..endIndex];
-            }
-
-            // update the new start for next time-tag calculation.
-            startIndex = endIndex + match.Length;
-
-            // add the time-tag.
-            var hasText = startIndex < endTextIndex;
-            var textIndex = lyric.Text.Length - (hasText ? 0 : 1);
-            var state = hasText ? IndexState.Start : IndexState.End;
-            var time = TimeTagUtils.TimeTagToMillionSecond(match.Value);
-            lyric.TimeTags.Add(new TextIndex(textIndex, state), time);
-        }
-
-        // should add remaining text at the right of the end time-tag.
-        lyric.Text += text[startIndex..endTextIndex];
-
-        return lyric;
+            Text = lyric,
+            TimeTags = timeTags
+        };
     }
 
     public override string Encode(LrcLyric component, int index)
     {
-        var text = component.Text;
-
-        var insertIndex = 0;
-
-        foreach (var (textIndex, time) in component.TimeTags)
-        {
-            var timeTagString = TimeTagUtils.MillionSecondToTimeTag(time);
-            var stringIndex = TextIndexUtils.ToStringIndex(textIndex);
-            text = text.Insert(insertIndex + stringIndex, timeTagString);
-
-            insertIndex += timeTagString.Length;
-        }
-
-        return text;
+        return LrcTimedTextUtils.ToTimedText(component.Text, component.TimeTags);
     }
 }
