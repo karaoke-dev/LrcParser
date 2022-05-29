782bd4f46faf487b4a741920d2f05ffdea4463c2
andy840119 <andy840119@gmail.com>
2022-05-29 17:04:53 +0800

    remove un-need ctor.

--- LrcParser/Extension/EnumerableExtensions.cs
+++ LrcParser/Extension/EnumerableExtensions.cs
@@ -54,11 +54,6 @@ public static class EnumerableExtensions
 
 public class Grouping<TKey, TElement> : List<TElement>, IGrouping<TKey, TElement>
 {
-    public Grouping(TKey key) => Key = key;
-
-    public Grouping(TKey key, int capacity)
-        : base(capacity) => Key = key;
-
     public Grouping(TKey key, IEnumerable<TElement> collection)
         : base(collection) => Key = key;
 
